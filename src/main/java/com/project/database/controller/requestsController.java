d9844c61be37833e83dc0de409a90c02ebc48624
Shubham Kumar <edwssm777@gmail.com>
2021-04-09 23:50:08 +0530

    changing cors

--- src/main/java/com/project/database/controller/requestsController.java
+++ src/main/java/com/project/database/controller/requestsController.java
@@ -14,8 +14,10 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 @RestController
+@CrossOrigin(origins = "*")
 public class requestsController {
     
     @Autowired
